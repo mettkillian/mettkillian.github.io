a06aeed614ef4a840c6f2acceee483ee2e864ce7
Dainon Snow Account <dainon@byui.edu>
2019-03-01 17:04:56 -0700

    form

---
+++ assignments/lesson-8/scripts/map.js
@@ -0,0 +1,8 @@
+
+
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(42.0963,-111.8766)
+      };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
