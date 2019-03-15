44be61e6280f26ee80ab5154793071da0740e06c
Dainon Snow Account <dainon@byui.edu>
2019-03-15 13:30:29 -0600

    preston 10

---
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -0,0 +1,77 @@
+
+let weatherRequest = new XMLHttpRequest();
+
+weatherRequest.open('GET', "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=86ba4d2c72ca8e279c8006007e3ef735", true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    let t = [weatherData.main.temp];
+    let w = [weatherData.wind.speed];
+    let h = [weatherData.main.humidity];
+    let s = [weatherData.weather[0].main];
+
+    t = Math.round(t);
+    w = Math.round(w);
+  
+    document.getElementById("current-temp").textContent=t;
+    document.getElementById("current-speed").textContent=w;
+    document.getElementById("current-humidity").textContent=h;
+    var windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(w, 0.16) + 0.4275 * t * Math.pow(w, 0.16);
+    windChill = Math.round(windChill);
+    document.getElementById("current-chill").textContent=windChill;
+    document.getElementById("current-skies").textContent=s;
+    
+
+}
+
+let forecastRequest = new XMLHttpRequest();
+
+forecastRequest.open('GET', "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=86ba4d2c72ca8e279c8006007e3ef735", true);
+forecastRequest.send();
+
+forecastRequest.onload = function() {
+    let forecastData = JSON.parse(forecastRequest.responseText);
+    console.log(forecastData);
+    
+        tempry = [];
+        datery = [];
+        iconry = [];
+
+    for (i=1; i<forecastData.list.length; i++) {
+        dtext=forecastData.list[i].dt_txt;
+        if (dtext.includes("06:00:00")) {
+            let temperature = forecastData.list[i].main.temp;
+            tempry.push(temperature);
+            let date = new Date(forecastData.list[i].dt*1000); 
+            var wday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var currentdate= wday[date.getDay()];
+            datery.push(currentdate);
+            var iconcode = forecastData.list[i].weather["0"].icon;
+            var iconimg = "https://openweathermap.org/img/w/"+ iconcode +".png";
+            iconry.push(iconimg);
+        }  
+        continue;
+    }
+
+    document.getElementById("fcth1").innerHTML = datery[0];
+    document.getElementById("fcth2").innerHTML = datery[1];
+    document.getElementById("fcth3").innerHTML = datery[2];
+    document.getElementById("fcth4").innerHTML = datery[3];
+    document.getElementById("fcth5").innerHTML = datery[4];
+
+    document.getElementById("tdicon1").innerHTML = iconry[0];
+    document.getElementById("tdicon2").innerHTML = iconry[1];
+    document.getElementById("tdicon3").innerHTML = iconry[2];
+    document.getElementById("tdicon4").innerHTML = iconry[3];
+    document.getElementById("tdicon5").innerHTML = iconry[4];
+
+    document.getElementById('tdtemp1').innerHTML = tempry[0];
+    document.getElementById('tdtemp2').innerHTML = tempry[1];
+    document.getElementById('tdtemp3').innerHTML = tempry[2];
+    document.getElementById('tdtemp4').innerHTML = tempry[3];
+    document.getElementById('tdtemp5').innerHTML = tempry[4];
+
+}
--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -1,6 +1,6 @@
 
-var temp = parseInt(document.getElementById('Temperature').innerHTML);
-var speed = parseInt(document.getElementById('Wind').innerHTML);
+var temp = parseInt(document.getElementById('current-temp').innerHTML);
+var speed = parseInt(document.getElementById('current-speed').innerHTML);
 var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
 windChill = Math.round(windChill);
-document.getElementById('windChill').innerHTML = windChill;
+document.getElementById('current-chill').textContent = windChill;
