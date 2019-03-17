e5c0c1288ba5ebdb91a9e154da3826ffda40027d
Dainon Snow Account <dainon@byui.edu>
2019-03-16 23:28:01 -0600

    town events

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -76,3 +76,33 @@ forecastRequest.onload = function() {
     document.getElementById('tdtemp5').innerHTML = tempry[4] + "&deg";
 
 }
+
+    
+
+    var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function () {
+    var townData = request.response;
+    showData(townData);
+}
+
+    function showData(jsonObj) {
+        var data = jsonObj['towns'];
+         for (var i = 0; i < data.length; i++) {
+            var name = data[i].name;
+            if ((name.includes("Preston")) == false) {
+            continue;
+        }
+       
+        var even=[]
+        var townEvents = data[i].events;       
+        
+        }
+            document.getElementById('output1').innerHTML=townEvents[0];
+            document.getElementById('output2').innerHTML=townEvents[1];
+            document.getElementById('output3').innerHTML=townEvents[2];   
+    
+    }
